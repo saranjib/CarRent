8e2b9959d5bd5723c18002f959558a77b436a2d3
saranjib <skuanar1992@hotmail.com>
2025-10-17 19:36:39 +0530

    Some frontend file update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { use, useState } from 'react'
 import {navbarStyles as styles} from '../assets/dummyStyles'
+import Logo from '../assets/logocar.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -11,9 +13,24 @@ const Navbar = () => {
     { to: '/contact', label: 'Contact' },
   ];
     return (
-        <nav className= {'${styles.base} ${scrolled}'}>
+        <nav className= {'${styles.base} ${scrolled ? styles.nav.scrolled : styles.nav.notScrolled} }'}
+        aria-label="Main Navigation">
+        <div className=" max-w-7xl mx-auto pxx-4 sm:px-6 lg:px-8">
+            <div className="flex justify-center">
+                <div className={'${styles.floatingNav.scrolled : styles.flotingNav.notScrolled} '}
+                    role="region" aria-roledescription="navigation">
+                  <div className="flex items-center justify-between gap-4">
+                    <Link className="flex items-center">
+                    <div className={styles.logoContainer}>
+                        <img src={Logo} alt= "Logo" style= {{ display: "block", objectFit: "contain" }}/>
+                    </div>
+                    </Link>
+                  </div>      
+                 </div>
+                </div>
+             </div>
             </nav>
-  )
-}
+  );
+};
 
 export default Navbar
